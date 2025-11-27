5d3e23abcbc5276cdf39f14bbf3d97f155123e32
ThNam2310 <namkim102@gmail.com>
2025-11-27 16:01:44 +0700

    Fix

--- frontend/resources/views/cart.blade.php
+++ frontend/resources/views/cart.blade.php
@@ -53,124 +53,124 @@
                 </div>
             </div>
         </div>
-        <<<<<<< HEAD </div>
-            <!--====== End - Section Content ======-->
     </div>
-    <!--====== End - Section 2 ======-->
+    <!--====== End - Section Content ======-->
+</div>
+<!--====== End - Section 2 ======-->
 
 
-    <!--====== Section 3 ======-->
-    <div class="u-s-p-b-60">
+<!--====== Section 3 ======-->
+<div class="u-s-p-b-60">
 
-        <!--====== Section Content ======-->
-        <div class="section__content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 u-s-m-b-30">
-                        <form class="f-cart">
-                            <div class="row">
-                                <div class="col-lg-4 col-md-6 u-s-m-b-30">
-                                    <div class="f-cart__pad-box">
-                                        <h1 class="gl-h1">ESTIMATE SHIPPING AND TAXES</h1>
+    <!--====== Section Content ======-->
+    <div class="section__content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 u-s-m-b-30">
+                    <form class="f-cart">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-6 u-s-m-b-30">
+                                <div class="f-cart__pad-box">
+                                    <h1 class="gl-h1">ESTIMATE SHIPPING AND TAXES</h1>
 
-                                        <span class="gl-text u-s-m-b-30">Enter your destination to get a shipping
-                                            estimate.</span>
-                                        <div class="u-s-m-b-30">
+                                    <span class="gl-text u-s-m-b-30">Enter your destination to get a shipping
+                                        estimate.</span>
+                                    <div class="u-s-m-b-30">
 
-                                            <!--====== Select Box ======-->
+                                        <!--====== Select Box ======-->
 
-                                            <label class="gl-label" for="shipping-country">COUNTRY *</label><select
-                                                class="select-box select-box--primary-style" id="shipping-country">
-                                                <option selected value="">Choose Country</option>
-                                                <option value="uae">United Arab Emirate (UAE)</option>
-                                                <option value="uk">United Kingdom (UK)</option>
-                                                <option value="us">United States (US)</option>
-                                            </select>
-                                            <!--====== End - Select Box ======-->
-                                        </div>
-                                        <div class="u-s-m-b-30">
-
-                                            <!--====== Select Box ======-->
-
-                                            <label class="gl-label" for="shipping-state">STATE/PROVINCE *</label><select
-                                                class="select-box select-box--primary-style" id="shipping-state">
-                                                <option selected value="">Choose State/Province</option>
-                                                <option value="al">Alabama</option>
-                                                <option value="al">Alaska</option>
-                                                <option value="ny">New York</option>
-                                            </select>
-                                            <!--====== End - Select Box ======-->
-                                        </div>
-                                        <div class="u-s-m-b-30">
-
-                                            <label class="gl-label" for="shipping-zip">ZIP/POSTAL CODE *</label>
-
-                                            <input class="input-text input-text--primary-style" type="text"
-                                                id="shipping-zip" placeholder="Zip/Postal Code">
-                                        </div>
-                                        <div class="u-s-m-b-30">
-
-                                            <a class="f-cart__ship-link btn--e-transparent-brand-b-2"
-                                                href="{{ route('cart') }}">CALCULATE SHIPPING</a>
-                                        </div>
-
-                                        <span class="gl-text">Note: There are some countries where free shipping is
-                                            available otherwise our flat rate charges or country delivery charges will
-                                            be apply.</span>
+                                        <label class="gl-label" for="shipping-country">COUNTRY *</label><select
+                                            class="select-box select-box--primary-style" id="shipping-country">
+                                            <option selected value="">Choose Country</option>
+                                            <option value="uae">United Arab Emirate (UAE)</option>
+                                            <option value="uk">United Kingdom (UK)</option>
+                                            <option value="us">United States (US)</option>
+                                        </select>
+                                        <!--====== End - Select Box ======-->
                                     </div>
-                                </div>
-                                <div class="col-lg-4 col-md-6 u-s-m-b-30">
-                                    <div class="f-cart__pad-box">
-                                        <h1 class="gl-h1">NOTE</h1>
+                                    <div class="u-s-m-b-30">
 
-                                        <span class="gl-text u-s-m-b-30">Add Special Note About Your Product</span>
-                                        <div>
+                                        <!--====== Select Box ======-->
 
-                                            <label for="f-cart-note"></label><textarea
-                                                class="text-area text-area--primary-style" id="f-cart-note"></textarea>
-                                        </div>
+                                        <label class="gl-label" for="shipping-state">STATE/PROVINCE *</label><select
+                                            class="select-box select-box--primary-style" id="shipping-state">
+                                            <option selected value="">Choose State/Province</option>
+                                            <option value="al">Alabama</option>
+                                            <option value="al">Alaska</option>
+                                            <option value="ny">New York</option>
+                                        </select>
+                                        <!--====== End - Select Box ======-->
                                     </div>
+                                    <div class="u-s-m-b-30">
+
+                                        <label class="gl-label" for="shipping-zip">ZIP/POSTAL CODE *</label>
+
+                                        <input class="input-text input-text--primary-style" type="text"
+                                            id="shipping-zip" placeholder="Zip/Postal Code">
+                                    </div>
+                                    <div class="u-s-m-b-30">
+
+                                        <a class="f-cart__ship-link btn--e-transparent-brand-b-2"
+                                            href="{{ route('cart') }}">CALCULATE SHIPPING</a>
+                                    </div>
+
+                                    <span class="gl-text">Note: There are some countries where free shipping is
+                                        available otherwise our flat rate charges or country delivery charges will
+                                        be apply.</span>
                                 </div>
-                                <div class="col-lg-4 col-md-6 u-s-m-b-30">
-                                    <div class="f-cart__pad-box">
-                                        <div class="u-s-m-b-30">
-                                            <table class="f-cart__table">
-                                                <tbody>
-                                                    <tr>
-                                                        <td>SHIPPING</td>
-                                                        <td>$4.00</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>TAX</td>
-                                                        <td>$0.00</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>SUBTOTAL</td>
-                                                        <td>$379.00</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>GRAND TOTAL</td>
-                                                        <td>$379.00</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                        <div>
-                                            <a href="{{ route('checkout') }}" class="btn btn--e-brand-b-2">
-                                                PROCEED TO CHECKOUT
-                                            </a>
-                                        </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 u-s-m-b-30">
+                                <div class="f-cart__pad-box">
+                                    <h1 class="gl-h1">NOTE</h1>
+
+                                    <span class="gl-text u-s-m-b-30">Add Special Note About Your Product</span>
+                                    <div>
+
+                                        <label for="f-cart-note"></label><textarea
+                                            class="text-area text-area--primary-style" id="f-cart-note"></textarea>
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                            <div class="col-lg-4 col-md-6 u-s-m-b-30">
+                                <div class="f-cart__pad-box">
+                                    <div class="u-s-m-b-30">
+                                        <table class="f-cart__table">
+                                            <tbody>
+                                                <tr>
+                                                    <td>SHIPPING</td>
+                                                    <td>$4.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>TAX</td>
+                                                    <td>$0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>SUBTOTAL</td>
+                                                    <td>$379.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>GRAND TOTAL</td>
+                                                    <td>$379.00</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div>
+                                        <a href="{{ route('checkout') }}" class="btn btn--e-brand-b-2">
+                                            PROCEED TO CHECKOUT
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <!--====== End - Section Content ======-->
     </div>
-    <!--====== End - Section 3 ======-->
+    <!--====== End - Section Content ======-->
+</div>
+<!--====== End - Section 3 ======-->
 </div>
 @endsection
 {{-- 4. Kết thúc phần nội dung --}}
